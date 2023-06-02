5a51ab19403db8eadc4ac31da835bef1c5b29c8f
danidsnk <mdesinka@gmail.com>
2023-06-02 19:26:47 +0600

    REPL config

--- lua/plugins/ironnvim.lua
+++ lua/plugins/ironnvim.lua
@@ -3,6 +3,7 @@ return {
     config = function()
         local iron = require("iron.core")
 
+        vim.keymap.set('v', '<leader>sc', function() iron.visual_send(); iron.send(nil, '\n') end, { silent = true, noremap = true })
         iron.setup {
             config = {
                 -- Whether a repl should be discarded or not
@@ -17,23 +18,27 @@ return {
                         command = function(meta)
                             local filename = vim.api.nvim_buf_get_name(meta.current_bufnr)
                             local cmd = '__file__ = r"' .. filename .. '";exec(open(r"' .. filename .. '").read())'
-                            return { 'python', '-i', '-c', cmd }
+                            local python_exe = 'python3'
+                            if vim.fn.executable('python') == 1 then
+                                python_exe = 'python'
+                            end
+                            return { python_exe, '-i', '-c', cmd }
                         end
                     },
                 },
                 repl_open_cmd = "vertical botright 80 split"
             },
             keymaps = {
-                send_motion = "<space>sc",
-                visual_send = "<space>sc",
+                --send_motion = "<space>sc",
+                --visual_send = "<space>sc",
                 send_file = "<space>sf",
                 send_line = "<space>sl",
-                send_mark = "<space>sm",
-                mark_motion = "<space>mc",
-                mark_visual = "<space>mc",
-                remove_mark = "<space>md",
-                cr = "<space>s<cr>",
-                interrupt = "<space>s<space>",
+                --send_mark = "<space>sm",
+                --mark_motion = "<space>mc",
+                --mark_visual = "<space>mc",
+                --remove_mark = "<space>md",
+                --cr = "<space>sc",
+                --interrupt = "<space>s<space>",
                 exit = "<space>sq",
                 clear = "<space>cl",
             },
