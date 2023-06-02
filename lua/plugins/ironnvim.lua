1ae4903340b6121172912fd88689dfe4759e2dfc
danidsnk <mdesinka@gmail.com>
2023-06-02 21:08:06 +0600

    REPL config

--- lua/plugins/ironnvim.lua
+++ lua/plugins/ironnvim.lua
@@ -3,7 +3,9 @@ return {
     config = function()
         local iron = require("iron.core")
 
+        vim.keymap.set('n', '<leader>so', function() vim.cmd('IronRepl') end, { silent = true, noremap = true })
         vim.keymap.set('v', '<leader>sc', function() iron.visual_send(); iron.send(nil, '\n') end, { silent = true, noremap = true })
+
         iron.setup {
             config = {
                 -- Whether a repl should be discarded or not
@@ -20,6 +22,8 @@ return {
                             local cmd = '__file__ = r"' .. filename .. '";exec(open(r"' .. filename .. '").read())'
                             local python_exe = 'python3'
                             if vim.fn.executable('python') == 1 then
+                                -- for windows
+                                vim.keymap.set('n', '<leader>sq', function() iron.send(nil, 'exit()\n') end, { silent = true, noremap = true })
                                 python_exe = 'python'
                             end
                             return { python_exe, '-i', '-c', cmd }
@@ -31,16 +35,16 @@ return {
             keymaps = {
                 --send_motion = "<space>sc",
                 --visual_send = "<space>sc",
-                send_file = "<space>sf",
-                send_line = "<space>sl",
+                --send_file = "<space>sf",
+                send_line = "<leader>sl",
                 --send_mark = "<space>sm",
                 --mark_motion = "<space>mc",
                 --mark_visual = "<space>mc",
                 --remove_mark = "<space>md",
                 --cr = "<space>sc",
-                --interrupt = "<space>s<space>",
-                exit = "<space>sq",
-                clear = "<space>cl",
+                interrupt = "<leader>si",
+                exit = "<leader>sq",
+                clear = "<leader>scl",
             },
             highlight = {
                 italic = true
