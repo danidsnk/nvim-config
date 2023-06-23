8c0a075c558f527269b1e589c9cac7960f2dcd48
danidsnk <mdesinka@gmail.com>
2023-06-23 19:30:44 +0600

    add plugin for ChatGPT

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -49,8 +49,21 @@ return {
             require('Comment').setup()
         end
     },
+    {
         'skywind3000/asyncrun.vim',
         lazy = false,
+    },
+    {
+        "jackMort/ChatGPT.nvim",
+        event = "VeryLazy",
+        config = function()
+            require("chatgpt").setup()
+        end,
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope.nvim"
+        }
     },
     -- Copilot configuration
     {
