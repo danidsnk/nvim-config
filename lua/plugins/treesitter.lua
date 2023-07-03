13ae4bd9edb640cb09f40befc9ddc5143bcbba06
danidsnk <mdesinka@gmail.com>
2023-07-03 11:53:57 +0600

    colors from vscode

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -23,7 +23,11 @@ return {
             -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
             extended_mode = false, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
             max_file_lines = nil,  -- Do not enable for files with more than n lines, int
-            -- colors = {}, -- table of hex strings
+            colors = {
+                '#F1D700',         -- yellow
+                '#DA70D6',         -- purple
+                '#179FFF',         -- blue
+            },
             -- termcolors = {} -- table of colour name strings
         },
         indent = { enable = true },
