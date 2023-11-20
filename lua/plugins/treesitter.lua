6bd8504c045740d321196a0af41e39d0447f18bb
danidsnk <mdesinka@gmail.com>
2023-11-20 18:05:36 +0600

    add treesitter-textobjects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,6 +3,7 @@ return {
     dependencies = {
         'p00f/nvim-ts-rainbow',
         'nvim-treesitter/playground',
+        'nvim-treesitter/nvim-treesitter-textobjects'
     },
     lazy = false,
     opts = {
@@ -32,6 +33,50 @@ return {
         },
         indent = { enable = true },
         playground = { enable = false },
+        textobjects = {
+            select = {
+                enable = true,
+
+                -- Automatically jump forward to textobj, similar to targets.vim
+                lookahead = true,
+
+                keymaps = {
+                    ["a="] = { query = "@assignment.outer",
+                        desc = "Select outer part of an assignment" },
+                    ["i="] = { query = "@assignment.inner",
+                        desc = "Select inner part of an assignment" },
+                    ["l="] = { query = "@assignment.lhs",
+                        desc = "Select left hand side of an assignment" },
+                    ["r="] = { query = "@assignment.rhs",
+                        desc = "Select right hand side of an assignment" },
+
+                    ["aa"] = { query = "@parameter.outer",
+                        desc = "Select outer part of a parameter/argument" },
+                    ["ia"] = { query = "@parameter.inner",
+                        desc = "Select inner part of a parameter/argument" },
+
+                    ["ai"] = { query = "@conditional.outer",
+                        desc = "Select outer part of a conditional" },
+                    ["ii"] = { query = "@conditional.inner",
+                        desc = "Select inner part of a conditional" },
+
+                    ["al"] = { query = "@loop.outer",
+                        desc = "Select outer part of a loop" },
+                    ["il"] = { query = "@loop.inner",
+                        desc = "Select inner part of a loop" },
+
+                    ["af"] = { query = "@function.outer",
+                        desc = "Select outer part of a method/function definition" },
+                    ["if"] = { query = "@function.inner",
+                        desc = "Select inner part of a method/function definition" },
+
+                    ["ac"] = { query = "@class.outer",
+                        desc = "Select outer part of a class" },
+                    ["ic"] = { query = "@class.inner",
+                        desc = "Select inner part of a class" },
+                },
+            },
+        },
     },
     config = function(_, opts)
         require('nvim-treesitter.configs').setup(opts)
